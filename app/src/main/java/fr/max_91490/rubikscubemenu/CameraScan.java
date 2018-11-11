b6d70d2cd07499d6fa1fb572c339218d6b8b8b28
Maxime <maximeblanchon91490@gmail.com>
2018-11-11 22:43:06 +0100

    solver new layout + onclic effect

--- app/src/main/java/fr/max_91490/rubikscubemenu/CameraScan.java
+++ app/src/main/java/fr/max_91490/rubikscubemenu/CameraScan.java
@@ -5,14 +5,10 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class CameraScan extends AppCompatActivity {
 
--- app/src/main/java/fr/max_91490/rubikscubemenu/SolveFragment.java
+++ app/src/main/java/fr/max_91490/rubikscubemenu/SolveFragment.java
@@ -7,30 +7,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SolveFragment extends Fragment {
 
+    View view;
 
     public SolveFragment() {
         // Required empty public constructor
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_solve, container, false);
+        view = inflater.inflate(R.layout.fragment_solve, container, false);
 
-        Button btn = (Button) view.findViewById(R.id.btn_scan);
-        btn.setOnClickListener(new View.OnClickListener() {
+        final AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
+        final PlayFragment playFragment = new PlayFragment();
+
+        final Button button1 = (Button)view.findViewById(R.id.button3);
+        final Button button2 = (Button)view.findViewById(R.id.button4);
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
+                v.startAnimation(buttonClick);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                v.startAnimation(buttonClick);
                 Intent in = new Intent(getActivity(), CameraScan.class);
- //               in.putExtra("some", "som data");
+                //in.putExtra("some", "som data");
                 startActivity(in);
             }
         });
