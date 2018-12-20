27af47874134703ee13f88adf431c1fd6e9b8a9e
Maxime <maximeblanchon91490@gmail.com>
2018-12-20 11:57:40 +0000

    Début création packages

--- app/src/main/java/fr/max_91490/rubikscubemenu/rotation/RotationComplex.java
+++
@@ -1,8 +0,0 @@
-package fr.max_91490.rubikscubemenu.rotation;
-
-import fr.max_91490.rubikscubemenu.cube.Cube;
-
-public interface RotationComplex extends Rotation
-{
-    public void rotate(Cube cube, int direction, int index);
-}
