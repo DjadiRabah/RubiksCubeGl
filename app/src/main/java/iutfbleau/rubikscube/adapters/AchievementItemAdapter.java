bc80012a35c8776c7244a04a3c8564f9b228d2d0
Maxime <maximeblanchon91490@gmail.com>
2019-01-26 18:31:15 +0100

    début affichage des succès

---
+++ app/src/main/java/iutfbleau/rubikscube/adapters/AchievementItemAdapter.java
@@ -0,0 +1,99 @@
+package iutfbleau.rubikscube.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import iutfbleau.rubikscube.R;
+import iutfbleau.rubikscube.model.AchievementItem;
+
+public class AchievementItemAdapter extends BaseAdapter {
+
+    // fields
+
+    private Context context;
+    private List<AchievementItem> achievementItemList;
+    private LayoutInflater inflater;
+
+    //constructor
+
+    public AchievementItemAdapter(Context context, List<AchievementItem> highTechItemList) {
+
+        this.context = context;
+        this.achievementItemList = highTechItemList;
+        this.inflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public int getCount() {
+
+        return achievementItemList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return achievementItemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = inflater.inflate(R.layout.adapter_item, null);
+
+        //get informations about item
+        AchievementItem currentItem = (AchievementItem) getItem(position);
+        final String itemName = currentItem.getName();
+        String mnemonic = currentItem.getMnemonic();
+        final String itemDesc = currentItem.getDesc();
+
+        //get image icon view
+        ImageView itemIconView = convertView.findViewById(R.id.item_icon);
+
+        //get item desc view
+        TextView itemPriceView = convertView.findViewById(R.id.item_desc);
+        itemPriceView.setText(itemDesc);
+
+        //get item name view
+        TextView itemNameView = convertView.findViewById(R.id.item_name);
+
+        if(currentItem.getState()){
+            String ressourceName = "item_" + mnemonic + "_icon";
+            int resId = context.getResources().getIdentifier(ressourceName, "drawable", context.getPackageName());
+            itemIconView.setImageResource(resId);
+            itemNameView.setText(itemName);
+            itemNameView.setTextColor(convertView.getResources().getColor(R.color.green));
+
+        }else{
+
+            int resId = context.getResources().getIdentifier(String.valueOf(R.drawable.item_lock_icon), "drawable", context.getPackageName());
+            itemIconView.setImageResource(resId);
+            itemNameView.setText(itemName + " [Locked]");
+            itemNameView.setTextColor(convertView.getResources().getColor(R.color.red));
+
+        }
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Achievement : "+itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return convertView;
+    }
+}
+
