0a898e3e16df98583dd2638740fb5678927e1576
djadi <Djadi.Rabah.Fr@gmail.com>
2019-03-07 19:18:19 +0100

    reverse rotation

--- app/src/main/java/iutfbleau/rubikscube/event/RotationEvent.java
+++ app/src/main/java/iutfbleau/rubikscube/event/RotationEvent.java
@@ -1,20 +1,43 @@
 package iutfbleau.rubikscube.event;
 
+import iutfbleau.rubikscube.models.rotation.Rotation;
+
 public class RotationEvent implements Event
 {
 	private int direction;
+	private int reverseDirection;
 	private int index;
 	public RotationEvent(int direction, int index) 
 	{
 		this.direction = direction;
+		this.setReverseDirection(direction);
 		this.index = index;
 	}
-	
+
+	private void setReverseDirection(int direction)
+	{
+		if(direction == Rotation.LEFT)
+			this.reverseDirection = Rotation.RIGHT;
+		else if(direction == Rotation.RIGHT)
+			this.reverseDirection = Rotation.LEFT;
+		else if(direction == Rotation.UP)
+			this.reverseDirection = Rotation.DOWN;
+		else if(direction == Rotation.DOWN)
+			this.reverseDirection = Rotation.UP;
+		else if(direction == Rotation.CLOCKWISE)
+			this.reverseDirection = Rotation.COUNTERCLOCKWISE;
+		else if(direction == Rotation.COUNTERCLOCKWISE)
+			this.reverseDirection = Rotation.CLOCKWISE;
+	}
+
 	public int getDirection()
 	{
 		return this.direction;
 	}
-	
+	public int getReverseDirection()
+	{
+		return this.reverseDirection;
+	}
 	public int getIndex()
 	{
 		return this.index;
